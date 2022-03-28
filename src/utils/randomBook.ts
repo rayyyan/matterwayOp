62c5566f38168ca1e92798d1f4a457c20f789422
ibenchi <horizen27@gmail.com>
2022-03-28 12:01:02 +0100

    randomBook desc

--- src/utils/randomBook.ts
+++ src/utils/randomBook.ts
@@ -1,3 +1,8 @@
+/**
+ * @function randomBook
+ * @param myArray Array of strings
+ * @returns {string} Random element from array
+ */
 export const randomBook = (myArray: string[]): string => {
   return myArray[Math.floor(Math.random() * myArray.length)]
 }
